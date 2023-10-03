f53af59caf013ad428fc39c17c39a75b66e5c563
iggodinho <igorgt30@gmail.com>
2023-10-03 00:37:35 -0300

    arquivo de chamadas

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "src/mylib.hpp"
+#include <iostream>
+#include <string>
+
+int main() {
+    Graph my_graph((std::string)"grafo.txt", 'v'); // Cria a instância e atribue a uma variável // v=vetor, m=matriz
+    my_graph.CalculateGraphStats(); 
+    my_graph.ConnectedComponents();
+    my_graph.BFS(1, 1, 0, 1, 1);
+    my_graph.DFS(1);
+    my_graph.Distance(1, 2);
+    my_graph.PrintRepresentation();
+   
+  
+    return 0;
+}
